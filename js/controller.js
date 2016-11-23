bcff29832d4ff15898c70b49da63feacebc9a87b
juliakimchung <julhkm@gmail.com>
2016-11-23 09:57:44 -0600

    keypress, click events for controller.js, made test function on model.js

--- js/controller.js
+++ js/controller.js
@@ -1,24 +1,84 @@
 'use strict';
 
 // Requires
-require("./favicon");
-require("../sass/style.scss");
-let Model = require("./model");
+let fav = require("./favicon");
+let sass = require("../sass/style.scss");
+let model = require("./model");
 
-    // Input Elements
-let navSearch      = $("#nav-Search"),
-    navToWatch     = $("#nav-ToWatch"),
-    //link elements
-    navSignUp      = $("#nav-SignUp"),
-    navLogIn       = $("#nav-LogIn"),
-    navLogOut      = $("#nav-LogOut"),
-    showTracked    = $("show-tracked"),
-    showWatched    = $("show-watched"),
-    // buttons
-    searchOmdb     = $("search-omdb"),
-    searchFb       = $("search-fb");
+		// Input Elements
+let navSearch      = $("#nav-Search"), //keypress event
+				addToWatchList = $("add-toWatch"),
+				navToWatch     = $("#nav-ToWatch"),
+				navSignUp      = $("#nav-SignUp"),
+				navLogIn       = $("#nav-LogIn"),
+				navLogOut      = $("#nav-LogOut"),
+				showUntracked		= $("#show-untracked"),
+				showUnwatched  = $("show-unwatched"),
+				showWatched    = $("show-watched"),
+				showFavorites		= $("show-favorites");
+				// searchOmdb     = $("search-omdb"),
+				// searchFb       = $("search-fb");
 
-// Call needed functions in Model based on inputs
+console.log("it's working");
+	 
+		model.logIn();
+		let sign = model.logIn();
+		console.log("sign",sign );
 
-let test = Model.ModeltestFunction();
-console.log("From Controller:", test);
+	let activateEvents= function(){ 
+		//console.log("activateEvents", activateEvents);
+	navLogIn.click(function(event) {
+		model.logIn()
+			.then(function(result){
+				let user = result.user;
+				navLogIn.addClass("hide");
+				navLogOut.removeClass('hide');
+			});
+});
+
+
+	navLogOut.click(function(event) {
+		model.logOut()
+		.then(function() {
+				navLogIn.removeClass("hide");
+				navLogOut.addClass("hide");
+				$("row-container").html("");
+		});
+	});
+
+	navSearch.keypress(function(event) {
+		if(event.keycode === 13) {
+			let searchResult = $("nav-Search").val();
+			model.searchOmdb(searchResult);
+			$("nav-Search").val('');
+		}	
+});
+
+$(document).on("click", ".add-toWatch",(event) => {
+		let addId = event.target.id;
+			console.log("addId",addId );
+		model.searchId(addId)
+		.then((movieObj) => {
+				model.addToFirebase(movieObj);
+		});
+});
+
+	addToWatchList.click(function(event) {
+		model.addToFirebase();
+	});
+
+showUntracked.click(function(event) {
+	model.showUntracked();
+});
+
+showWatched.click(function(event) {
+		model.showWatched();
+});
+
+showFavorites.click(function(event) {
+		model.showFavorites();
+	});
+};
+activateEvents();
+
+module.exports = {activateEvents};
--- js/model.js
+++ js/model.js
@@ -8,14 +8,27 @@ let Render = require("./view");
 // write functions that are called based on inputs in controller. They use view,
 
 let ModeltestFunction = () => "I was created in the Model";
-
-let test2 = Render.viewTestFunction();
+let showUntracked = () => " Show untracked movie list";
+let test2 = showUntracked();
 console.log("From Model:", test2);
 
 let test3 = Firebase.firebaseTestFunction();
 console.log("From Model:", test3);
 
-let test4 = OpenMovie.openMovieTestFunction();
-console.log("From Model:", test4);
+// let test4 = OpenMovie.openMovieTestFunction();
+// console.log("From Model:", test4);
+// function showUntracked(event){
+// 	return event;
+// }
+// showUntracked();
+// let test = Render.showUntracked();
+// console.log("test", test);
 
-module.exports = {ModeltestFunction}; // logic functions for controllers
+let logIn = function(){
+	return "this is a log in place";
+};
+//let test4 = logIn();
+console.log("test", logIn() );
+
+
+module.exports = {showUntracked, logIn}; // logic functions for controllers
