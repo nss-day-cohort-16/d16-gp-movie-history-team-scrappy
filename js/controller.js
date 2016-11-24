584ee41137b0846a0278d03948b85fbd6b054f8a
juliakimchung <julhkm@gmail.com>
2016-11-23 21:53:33 -0600

    all event click button working well

--- js/controller.js
+++ js/controller.js
@@ -6,94 +6,85 @@ let sass = require("../sass/style.scss");
 let model = require("./model");
 
 		// Input Elements
-let navSearch      = $("#nav-Search"), //keypress event
-				addToWatchList = $("#add-toWatch"),
-				//navToWatch     = $("#nav-ToWatch"),
-				//navSignUp      = $("#nav-SignUp"),
-				navLogIn       = $("#nav-LogIn"),
-				navLogOut      = $("#nav-LogOut"),
-				showUntracked		= $("#show-untracked"),
-				showUnwatched  = $("#show-unwatched"),
-				showWatched    = $("#show-watched"),
-				showFavorites		= $("#show-favorites");
-				// searchOmdb     = $("search-omdb"),
-				// searchFb       = $("search-fb");
-
-console.log("it's working");
+let navSearch   = $("#nav-Search"), //keypress event
+		addToWatchList = $("#add-toWatch"),
+		navLogIn       = $("#nav-LogIn"),
+		navLogOut      = $("#nav-LogOut"),
+		showUntracked  = $("#show-untracked"),
+		showUnwatched  = $("#show-unwatched"),
+		showWatched    = $("#show-watched"),
+		showFavorites  = $("#show-favorites");
+		
+		console.log("it's working");
 	 
-		model.logIn();
-		let sign = model.logIn();
-		console.log("sign",sign );
-
-	let activateEvents= function(){ 
-		//console.log("activateEvents", activateEvents);
+let activateEvents= function(){ 
+		
 	navLogIn.click(function(event) {
-		console.log("event",event );
-		model.logIn()
-			.then(function(result){
-				let user = result.user;
-				navLogIn.addClass("hide-out");
-				navLogOut.removeClass('hide-out');
-			});
+	console.log("you clicked the login button",event );
+	model.logIn()
+	.then(function(result){
+		let user = result.user;
+		navLogIn.addClass("hide-out");
+		navLogOut.removeClass('hide-out');
+	});
 });
 
 
 	navLogOut.click(function(event) {
-		console.log("event",event );
-		model.logOut()
-		.then(function() {
-				navLogIn.removeClass("hide-out");
-				navLogOut.addClass("hide-out");
-				$("row-container").html("");
+	console.log("you clicked the logout button",event );
+	model.logOut()
+	.then(function() {
+		navLogIn.removeClass("hide-out");
+		navLogOut.addClass("hide-out");
+		$("row-container").html("");
 		});
 	});
 
 	navSearch.keypress(function(event) {
-		console.log(event);
-		if(event.keycode === 13) {
-			let searchResult = $("nav-Search").val();
-			model.searchOmdb(searchResult);
-			$("nav-Search").val('');
+	console.log("you clicked the search keypress", event);
+	if(event.keycode === 13) {
+		let searchResult = $("nav-Search").val();
+		model.searchOmdb(searchResult);
+		$("nav-Search").val('');
 		}	
 });
 
 $(document).on("click", ".add-toWatch",(event) => {
-	console.log("event", event );
-		let addId = event.target.id;
-			console.log("addId",addId );
-		model.searchId(addId)
+	console.log("you clicked add to watchlist", event );
+	let addId = event.target.id;
+	model.searchId(addId)
 		.then((movieObj) => {
-				model.addToFirebase(movieObj);
+		 model.addToFirebase(movieObj);
 		});
 });
 
-	addToWatchList.click(function(event) {
-		console.log("event", event );
+addToWatchList.click(function(event) {
+		console.log("you clicked add to watchlist", event );
 		model.addToFirebase();
 	});
 
-	showUntracked.click(function(event) {
-	console.log("event",event );
+showUntracked.click(function(event) {
+	console.log("you clicked show untracked ",event );
 	model.showUntracked();
 });
 
 showUnwatched.click(function(event) {
-	console.log("event",event );
+	console.log("you clicked show unwatched",event );
 
 		model.showUnwatched();
 });
-	showWatched.click(function(event) {
-	console.log("event",event );
+showWatched.click(function(event) {
+	console.log("you clicked show watched",event );
 
 		model.showWatched();
 });
 
-	showFavorites.click(function(event) {
-		console.log("event",event );
+showFavorites.click(function(event) {
+	console.log("click event",event );
 
 		model.showFavorites();
 	});
 };
+
 activateEvents();
 
-module.exports = {activateEvents};
