16c95ea96774f4bfc3ca5258d981c9806a5b64d0
Ben Marks <onodric@yahoo.com>
2016-11-27 16:41:02 -0600

    finished fiddly bits in model
    search functionality and redraws

--- js/model.js
+++ js/model.js
@@ -5,12 +5,214 @@ let OpenMovie = require("./open-movie-interactions");
 let Firebase = require("./firebase-interactions");
 let User = require("./user");
 let Render = require("./view");
-let resultAll = {Search: []},
+let searchResultAll = {Search: []},
+    userMovies = {Search: []},
     resultOMDb = {Search: []},
     resultUntracked = {Search: []},
-    resultTracked = {Search: []};
+    searchResultTracked = {Search: []},
+    location = '',
+    searchString = '';
 
 // write functions that are called based on inputs in controller. They use view,
+
+// compare(result1, result2) returns 1 json file of unique movies
+let compare = (omdb, fb) => {
+  let tempIds = [];
+// Populate searchResultAll with fb results bc these are defaults
+  searchResultAll.Search.concat(fb.Search); // Will need an edit for the return value of FB search
+  for (let i = 0; i<fb.Search.length; i++){
+    tempIds.push(fb.Search[i].imdbID); // Will need an edit for the return value of FB search
+  }
+// compare omdb search results to tracked imdbIDs
+  for (let i = 0; i < omdb.Search.length; i++){
+    if (tempIds.includes(omdb.Search[i].imdbID)){
+      //skip it
+    } else {
+      resultUntracked.Search.push(omdb.Search[i]);
+      searchResultAll.Search.unshift(omdb.Search[i]);
+    }
+  }
+  return searchResultAll;
+};
+
+let checkLocation = function (str) {
+  switch (str){
+    case 'search':
+      searchAll(searchString);
+      break;
+    case 'untracked':
+      showUntracked();
+      break;
+    case 'unwatched':
+      showUnwatched();
+      break;
+    case 'watched':
+      showWatched();
+      break;
+    case 'favorites':
+      showFavorites();
+      break;
+  }
+};
+
+let userLocation = function(str){
+  location = str;
+};
+
+let loggedUser = () => {
+  if (User.getUser() === null){
+    return false;
+  } else {
+  return User.getUser();
+  }
+};
+
+// Call getUser() on page load, then call LoadPage(loggedIn).
+let logIn = () => {
+  User.logInGoogle();
+  landingPage(loggedUser());
+  Render.loadPage(loggedUser());
+};
+
+let logOut = () => {
+  User.logOut();
+  Render.loadPage(loggedUser());
+};
+
+let landingPage = (uid) => {
+  Firebase.getMovies(uid)
+  .then(function(movieData){
+    var idArr = Object.keys(movieData);
+    idArr.forEach(function (key) {
+      movieData[key].movieId = key;
+      userMovies.Search.push(movieData[key]);
+    });
+  });
+  // userMovies.Search = tempObj.Movies;
+};
+
+// Check if someone is logged in (calls loadPage with argument yes or no)
+Render.loadPage(loggedUser());
+
+// search(input): use getUser() then call Firebase.searchFirebase(input) if applicable and OpenMovies.getMovies(input), save input, and call signIn something, then call compare(), renderMovies(object, search)
+let searchAll = function(string){
+  location = 'search';
+  resultOMDb = OpenMovie.getMovies(string);
+  searchString = string.toLowerCase();
+  if (loggedUser()){
+    // searchResultTracked = Firebase.searchFirebase(string);
+    for (let i=0; i<userMovies.Search.length; i++){
+      for (let title in userMovies.Search[i]){
+        if (userMovies.Search[i].title.toLowerCase().contains(searchString)){
+          searchResultTracked.Search.push(userMovies.Search[i]);
+        }
+      }
+    }
+    searchResultAll = compare(resultOMDb, searchResultTracked);
+  } else {
+    searchResultAll = resultOMDb;
+  }
+  Render.renderMovies(searchResultAll, "search");
+};
+
+// showUntracked() run compare and return only results that do not have firebase uid, then call renderMovies(object, untracked)
+// resultUntracked is empty until we search, so this should display nothing unless first search is performed. As intended.
+let showUntracked = () => {
+  location = 'untracked';
+  Render.renderMovies(resultUntracked, "untracked");
+};
+
+// showUnwatched() will call getWatched(false), then call renderMovies(object, unwatched)
+let showUnwatched = () => {
+  location = 'unwatched';
+  let unwatched = {Search: []};
+  userMovies.Search.forEach(function (el, ind, arr) {
+    if (!el.watched){
+      unwatched.Search.push(el);
+    }
+  });
+  Render.renderMovies(unwatched, "unwatched");
+};
+
+// showWatched() will call getWatched(true), then call renderMovies(object, watched)
+let showWatched = () => {
+  location = 'watched';
+  let watched = {Search: []};
+  userMovies.Search.forEach(function (el, ind, arr) {
+    if (el.watched){
+      watched.Search.push(el);
+    }
+  });
+  Render.renderMovies(watched, "watched");
+};
+
+// showFavorites(), then call renderMovies(object, favorites)
+let showFavorites = () => {
+  location = 'favorites';
+  let fave = {Search: []};
+  userMovies.Search.forEach(function (el, ind, arr) {
+    if (parseInt(el.rating) === 10){
+      fave.Search.push(el);
+    }
+  });
+  Render.renderMovies(fave, "favorites");
+};
+
+// AddMovie(movieId) will call getFullMovie(imdbID), call Firebase.addMovie(object), call renderCard(object)
+let addMovie = (movieId) => {
+  let tempMovie = OpenMovie.getFullMovies(movieId);
+  let fullMovie = {
+    "year" : tempMovie.Year,
+    "actors" : tempMovie.Actors,
+    "title" : tempMovie.Title,
+    "watched" : false,
+    "rating" : 0,
+    "poster" : tempMovie.Poster,
+    "imdbID" : tempMovie.imdbID   
+  };
+  Firebase.addMovie(fullMovie);
+  Render.hideMovie(movieId);
+  landingPage(loggedUser());
+  checkLocation(location);
+  // Render.renderCard(fullMovie);
+};
+
+// DeleteMovie(movieId) will call hideMovie(),
+let deleteMovie = (movieId) => {
+  Firebase.deleteMovie(movieId);
+  Render.hideMovie(movieId);
+};
+
+// UpdateRating(movieId, rating) will change object then call renderCard(object) with new object
+// used to add to tracked (with add to watchlist) and add to watched (with rating)
+let updateMovie = (movieId, rating) => {
+  let tempMovie = function (ID) {
+    for (let i = 0; i<userMovies.Search.length; i++){
+      if (userMovies.Search[i].imdbID === ID){
+        return userMovies.Search[i];
+      }
+    }
+  };
+  let fullMovie = {
+    "year" : tempMovie.year,
+    "actors" : tempMovie.actors,
+    "title" : tempMovie.title,
+    "watched" : true,
+    "rating" : rating,
+    "poster" : tempMovie.poster,
+    "imdbID" : tempMovie.imdbID   
+  };
+  Firebase.editMovie(fullMovie, movieId);
+  let newMovie = Firebase.getMovie(movieId);
+  landingPage(loggedUser());
+  checkLocation(location);
+  // Render.renderCard(newMovie);
+};
+
+// addToWatchList(movieId) push new object to firebase will call renderCard(object),
+// REDUNDANT?
+// let addToWatchList = (obj, movieId) => {
+// };
 
 let ModeltestFunction = () => "I was created in the Model";
 
@@ -22,106 +224,6 @@ console.log("From Model:", test3);
 
 let test4 = OpenMovie.openMovieTestFunction();
 console.log("From Model:", test4);
-
-// compare(result1, result2) returns 1 json file of unique movies
-let compare = (omdb, fb) => {
-  let tempIds = [];
-// Populate resultAll with fb results bc these are defaults
-  resultAll.Search.concat(fb.Search); // Will need an edit for the return value of FB search
-  for (let i = 0; i<fb.Search.length; i++){
-    tempIds.push(fb.Search[i].imdbID); // Will need an edit for the return value of FB search
-  }
-// compare omdb search results to tracked imdbIDs
-  for (let i = 0; i < omdb.Search.length; i++){
-    if (tempIds.includes(omdb.Search[i].imdbID)){
-      //skip it
-    } else {
-      resultUntracked.Search.push(omdb.Search[i]);
-      resultAll.Search.unshift(omdb.Search[i]);
-    }
-  }
-  return resultAll;
-};
-
-// Call getUser() on page load, then call LoadPage(loggedIn).
-let logIn = () => {
-  User.logInGoogle();
-  Render.loadPage(loggedUser());
-};
-
-let logOut = () => {
-  User.logOut();
-  Render.loadPage(loggedUser());
-};
-
-let loggedUser = () => {
-  User.getUser();
-};
-
-// Check if someone is logged in (calls loadPage with argument yes or no)
-Render.loadPage(loggedUser());
-
-// search(input): use getUser() then call Firebase.searchFirebase(input) if applicable and OpenMovies.getMovies(input), save input, and call signIn something, then call compare(), renderMovies(object, search)
-let searchAll = function(string){
-  resultOMDb = OpenMovie.getMovies(string);
-  if (loggedUser()){
-    resultTracked = Firebase.searchFirebase(string);
-    resultAll = compare(resultOMDb, resultTracked);
-  } else {
-    resultAll = resultOMDb;
-  }
-  Render.renderMovies(resultAll, "search");
-};
-
-// showUntracked() run compare and return only results that do not have firebase uid, then call renderMovies(object, untracked)
-// resultUntracked is empty until we search, so this should display nothing unless first search is performed. As intended.
-let showUntracked = () => {
-  Render.renderMovies(resultUntracked, "untracked");
-};
-
-// showUnwatched() will call getWatched(false), then call renderMovies(object, unwatched)
-let showUnwatched = () => {
-  let unwatched = Firebase.getWatched(false);
-  Render.renderMovies(unwatched, "unwatched");
-};
-
-// showWatched() will call getWatched(true), then call renderMovies(object, watched)
-let showWatched = () => {
-  let watched = Firebase.getWatched(true);
-  Render.renderMovies(watched, "watched");
-};
-
-// showFavorites(), then call renderMovies(object, favorites)
-let showFavorites = () => {
-  let fave = Firebase.getFavorites();
-  Render.renderMovies(fave, "favorites");
-};
-
-// AddMovie(movieId) will call getFullMovie(imdbID), call Firebase.addMovie(object), call renderCard(object)
-let addMovie = (movieId) => {
-  let fullMovie = OpenMovie.getFullMovies(movieId);
-  Firebase.addMovie(fullMovie);
-  Render.renderCard(fullMovie);
-};
-
-// DeleteMovie(movieId) will call hideMovie(),
-let deleteMovie = (movieId) => {
-  Firebase.deleteMovie(movieId);
-  Render.hideMovie(movieId);
-};
-
-// UpdateRating(movieId, rating) will change object then call renderCard(object) with new object
-// used to add to tracked (with add to watchlist) and add to watched (with rating)
-let updateMovie = (obj, movieId) => {
-  Firebase.editMovie(obj, movieId);
-  let newMovie = Firebase.getMovie(movieId);
-  Render.renderCard(newMovie);
-};
-
-// addToWatchList(movieId) push new object to firebase will call renderCard(object),
-// REDUNDANT?
-// let addToWatchList = (obj, movieId) => {
-// };
 
 module.exports = {ModeltestFunction,
                   searchAll,
