815a0d13cfdfe65149ddeedd4f8da8937ba81b73
Nathan T. Baker <nathantbaker@gmail.com>
2016-11-28 07:22:20 -0600

    toggles inject dummy info

--- js/view.js
+++ js/view.js
@@ -22,48 +22,100 @@ let sectionStart     = $("#section-Start"),
     showUntracked    = $("#show-untracked"),
     showUnwatched    = $("#show-unwatched"),
     showWatched      = $("#show-watched"),
-    showFavorites    = $("#show-favorites");
+    showFavorites    = $("#show-favorites"),
+    // Search field
+    navSearch        = $("#nav-Search");
 
+// Display correct data in correct section
+let renderMovies = (movies, location) => {
+  switch (location){
+    case 'search':
+      searchInject.html("display search stuff here.");
+      break;
+    case 'untracked':
+      untrackedInject.html("display untracked stuff here.");
+      console.log("load untracked");
+      break;
+    case 'unwatched':
+      unwatchedInject.html("display unwatched stuff here.");
+      console.log("load unwatched");
+      break;
+    case 'watched':
+      watchedInject.html("display watched stuff here.");
+      console.log("load watched");
+      break;
+    case 'favorites':
+      favoritesInject.html("display favorites stuff here.");
+    console.log("load favorites");
+  }
+};
 
-// Show correct breadcrumbs when someone clicks enter within search bar or clicks a toggle button
+// CHANGE VIEW STATES
 
-// TOGGLING LOGIC
+// Search View
+navSearch.keypress( () => {
+  removeHelpText();
+});
+
+// Untracked View
+showUntracked.click(function() {
+  removeHelpText();
+  clearActiveToggle();
+  showUntracked.parent().addClass("active");
+  hideViewStates();
+  sectionUntracked.removeClass("hide");
+breadcrumbs.html("<h3>Movie History > Show Untracked</h3");
+});
+
+// Unwatched View
+showUnwatched.click(function() {
+  removeHelpText();
+  clearActiveToggle();
+  showUnwatched.parent().addClass("active");
+  hideViewStates();
+  sectionUnwatched.removeClass("hide");
+  breadcrumbs.html("<h3>Movie History > Show Unwatched</h3");
+});
+
+// Show Watched
+showWatched.click(function() {
+  removeHelpText();
+  clearActiveToggle();
+  showWatched.parent().addClass("active");
+  hideViewStates();
+  sectionWatched.removeClass("hide");
+  breadcrumbs.html("<h3>Movie History > Show Watched</h3");
+});
+
+// Favorites
+showFavorites.click(function() {
+  removeHelpText();
+  clearActiveToggle();
+  showFavorites.parent().addClass("active");
+  hideViewStates();
+  sectionFavorites.removeClass("hide");
+  breadcrumbs.html("<h3>Movie History > Favorites</h3");
+});
+
+// Base functions
 
 // Remove active class on all toggles before adding current one
 function clearActiveToggle() { $("label").removeClass("active"); }
 
+// Add hide to all view states
+function hideViewStates() {
+  sectionSearch.addClass("hide");
+  sectionUntracked.addClass("hide");
+  sectionUnwatched.addClass("hide");
+  sectionWatched.addClass("hide");
+  sectionFavorites.addClass("hide");
+}
 
-showUntracked.click(function() {
-clearActiveToggle();
-showUntracked.parent().addClass("active");
-breadcrumbs.html("<h3>Movie History > Show Untracked</h3");
-});
+// Remove helper text
+function removeHelpText() { sectionStart.addClass("hide"); }
 
-showUnwatched.click(function() {
-clearActiveToggle();
-showUnwatched.parent().addClass("active");
-breadcrumbs.html("<h3>Movie History > Show Unwatched</h3");
-});
-
-showWatched.click(function() {
-clearActiveToggle();
-showWatched.parent().addClass("active");
-breadcrumbs.html("<h3>Movie History > Show Watched</h3");
-});
-
-showFavorites.click(function() {
-clearActiveToggle();
-showFavorites.parent().addClass("active");
-breadcrumbs.html("<h3>Movie History > Favorites</h3");
-});
-
-
-// loadPage(loggedIn) takes yes or no. show sectoin-home OR section-unwatched
+// Show sectoin-home OR section-unwatched
 let loadPage = () => "I load a page";
-
-// renderMovies(object, state) state switches = favorites, watched, unwatched, untracked, search (hide correct toggles, display correct container, push results)
-let renderMovies = () => "I render movies";
-
 
 
 
