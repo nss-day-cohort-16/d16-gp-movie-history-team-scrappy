8d0f502251a19eb2c5a1a1d004cf37835605d58f
Nathan T. Baker <nathantbaker@gmail.com>
2016-11-27 22:20:30 -0600

    toggles keep active class, change breadcrumb

--- js/view.js
+++ js/view.js
@@ -3,20 +3,75 @@
 // Requires
 require('jquery');
 
-    // Pages To Toggle With "hide" class
-let sectionHome    = $("#section-Home"),
-    sectionSearch  = $("#section-Search"),
-    sectionToWatch = $("#section-ToWatch"),
-    sectionWatched = $("#section-Watched"),
-    // Places To Inject Content
-    homeInject     = $("#homeInject"),
-    searchInject   = $("#searchInject"),
-    watchedInject  = $("#watchedInject"),
-    toWatchInject  = $("#toWatchInject");
+    // Pages elements to toggle with "hide" class
+let sectionStart     = $("#section-Start"),
+    sectionSubNav    = $("#section-SubNav"),
+    sectionSearch    = $("#section-Search"),
+    sectionUntracked = $("#section-Untracked"),
+    sectionUnwatched = $("#section-Unwatched"),
+    sectionWatched   = $("#section-Watched"),
+    sectionFavorites = $("#section-Favorites"),
+    // Places to inject content
+    breadcrumbs      = $("#breadcrumbs"),
+    searchInject     = $("#searchInject"),
+    untrackedInject  = $("#untrackedInject"),
+    unwatchedInject  = $("#unwatchedInject"),
+    watchedInject    = $("#watchedInject"),
+    favoritesInject  = $("#favoritesInject"),
+    // Toggles
+    showUntracked    = $("#show-untracked"),
+    showUnwatched    = $("#show-unwatched"),
+    showWatched      = $("#show-watched"),
+    showFavorites    = $("#show-favorites");
 
-// Test
-$("#homeInject").html("<p>To get started tracking movies you'd like to watch, sign in at the top right, and then search for a movie name in the search bar above.</p>");
 
+// Show correct breadcrumbs when someone clicks enter within search bar or clicks a toggle button
+
+// TOGGLING LOGIC
+
+// Remove active class on all toggles before adding current one
+function clearActiveToggle() { $("label").removeClass("active"); }
+
+
+showUntracked.click(function() {
+clearActiveToggle();
+showUntracked.parent().addClass("active");
+breadcrumbs.html("<h3>Movie History > Show Untracked</h3");
+});
+
+showUnwatched.click(function() {
+clearActiveToggle();
+showUnwatched.parent().addClass("active");
+breadcrumbs.html("<h3>Movie History > Show Unwatched</h3");
+});
+
+showWatched.click(function() {
+clearActiveToggle();
+showWatched.parent().addClass("active");
+breadcrumbs.html("<h3>Movie History > Show Watched</h3");
+});
+
+showFavorites.click(function() {
+clearActiveToggle();
+showFavorites.parent().addClass("active");
+breadcrumbs.html("<h3>Movie History > Favorites</h3");
+});
+
+
+// loadPage(loggedIn) takes yes or no. show sectoin-home OR section-unwatched
+let loadPage = () => "I load a page";
+
+// renderMovies(object, state) state switches = favorites, watched, unwatched, untracked, search (hide correct toggles, display correct container, push results)
+let renderMovies = () => "I render movies";
+
+
+
+
+// hideMovie(movieID)
+let hideMovie = () => "I hide a movie";
+
+// Testing
 let viewTestFunction = () => "I was created in the View";
 
-module.exports = {viewTestFunction};
+module.exports = {loadPage, renderMovies, hideMovie, viewTestFunction};
+
